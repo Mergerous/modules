8e00e544fd6d25048e6939f3d7870f9eb8d22ba9
vladislav_kachanovich <>
2025-05-03 10:49:53 +0300

    fix

--- Assets/Scripts/Views/Samples/Elements/TransformElement.cs
+++ Assets/Scripts/Views/Samples/Elements/TransformElement.cs
@@ -1,4 +1,5 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Modules.Views
@@ -6,6 +7,8 @@ namespace Modules.Views
     [Serializable]
     public sealed class TransformElement : Element
     {
-        [field: SerializeField] public Transform Transform { get; private set; }
+        [SerializeField] private bool isRect;
+        [field: SerializeField, HideIf(nameof(isRect))] public Transform Transform { get; private set; }
+        [field: SerializeField, ShowIf(nameof(isRect))] public RectTransform RectTransform { get; private set; }
     }
 }
