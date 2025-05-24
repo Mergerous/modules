131b6c49552a0ed420727b3c44bd2452c2cece64
vladislav_kachanovich <>
2025-05-25 00:40:57 +0300

    fix

--- Assets/Scripts/Views/Samples/Presenters/ImagePresenter.cs
+++ Assets/Scripts/Views/Samples/Presenters/ImagePresenter.cs
@@ -1,26 +1,26 @@
 using JetBrains.Annotations;
 using R3;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
+// using UnityEngine.AddressableAssets;
 
 namespace Modules.Views
 {
     [UsedImplicitly]
     public sealed class ImagePresenter : Presenter
     {
-        public void SetSpriteAsync(View view, AssetReferenceSprite reference)
-        {
-            if (reference.IsDone)
-            {
-                view.GetElement<ImageElement>("image").SetSprite(reference.Asset as Sprite);
-            }
-            else
-            {
-                reference.LoadAssetAsync().Task
-                    .ToObservable()
-                    .Subscribe(sprite => view.GetElement<ImageElement>("image").SetSprite(sprite))
-                    .AddTo(disposables);
-            }
-        }
+        // public void SetSpriteAsync(View view, AssetReferenceSprite reference)
+        // {
+        //     if (reference.IsDone)
+        //     {
+        //         view.GetElement<ImageElement>("image").SetSprite(reference.Asset as Sprite);
+        //     }
+        //     else
+        //     {
+        //         reference.LoadAssetAsync().Task
+        //             .ToObservable()
+        //             .Subscribe(sprite => view.GetElement<ImageElement>("image").SetSprite(sprite))
+        //             .AddTo(disposables);
+        //     }
+        // }
     }
 }
