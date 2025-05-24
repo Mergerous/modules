4d678963dfc9f9b4f8700ff661ae2998034fe07b
vladislav_kachanovich <>
2025-05-25 00:42:09 +0300

    -

--- Assets/Scripts/Views/Samples/Elements/ImageElement.cs
+++ Assets/Scripts/Views/Samples/Elements/ImageElement.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-using UnityEngine.ResourceManagement.AsyncOperations;
+// using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
 namespace Modules.Views
