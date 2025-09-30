17ba6d31d318c812a29eb68f82486bfb4c53e3f1
vladislav_kachanovich <>
2025-09-30 21:36:41 +0300

    -

--- Assets/Scripts/Data/Runtime/DataConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Modules.Data
-{
-    public static partial class DataConstants
-    {
-        public const string DATA_PLAYER_PREFS_KEY = "player_prefs";
-        public const string DATA_PERSISTENS_DATA_KEY = "persistent_data";
-    }
-}
