a72931fea5768d4c40fe36c6e8e2b55b48b5eeaa
vladislav_kachanovich <>
2026-03-04 21:55:42 +0300

    -

--- Assets/Scripts/Audio/Runtime/AudioComponent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Modules.Audio.Components
-{
-    public class AudioComponent : MonoBehaviour
-    {
-        [field: SerializeField] public AudioSource Source { get; private set; }
-    }
-}
